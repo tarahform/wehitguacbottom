582e79e70270c5f529468a92b099b541793b5f62
tarahform <ptarahform@gmail.com>
2018-06-22 16:51:25 -0500

    view and components update

---
+++ client/src/Components/AlcoholListItem/AlcoholListItem.js
@@ -0,0 +1,8 @@
+import React from "react";
+
+const AlcoholListItem = ({ children }) => (
+  <div>
+  </div>
+);
+
+export default AlcoholListItem;
---
+++ client/src/Components/RecipeListItem/RecipeListItem.js
@@ -0,0 +1,8 @@
+import React from "react";
+
+const RecipeListItem = () => (
+  <div>
+  </div>
+);
+
+export default RecipeListItem;
