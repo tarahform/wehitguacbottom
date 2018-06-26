a41cf34cb6c49b04d72b34b0cb05a8666cb82281
tarahform <ptarahform@gmail.com>
2018-06-26 13:14:12 -0500

    user profile added

--- client/src/components/Signup/Signup.js
+++ client/src/components/Signup/Signup.js
@@ -2,7 +2,52 @@ import React from "react";
 import "./Signup.css";
 
 const Signup = () => (
-  <div><h1>Signup</h1>
+  <div class="container-fluid" id="signupPage">
+    <hr />
+    <div class="container-fluid" id="signupContainer">
+      <div class="row mt-3" id="signUpFormRow" data-aos="zoom-out-down">
+        <div class="col-md-10" id="signUpFormCol">
+          <div class="form-div">
+            <form id="signUpForm">
+              <div class="form-group">
+                <label for="name" id="firstNameLabel">First Name</label>
+                <input type="name" class="form-control" id="firstNameInput" rows="1" placeholder="John Doe" />
+              </div>
+              <div class="form-group">
+                <label for="name" id="middleNameLabel">Middle Name</label>
+                <input type="name" class="form-control" id="middleNameInput" rows="1" placeholder="John Doe" />
+              </div>
+              <div class="form-group">
+                <label for="name" id="lastNameLabel">Last Name</label>
+                <input type="name" class="form-control" id="lastNameInput" rows="1" placeholder="John Doe" />
+              </div>
+              <div class="form-group">
+                <label for="number" id="ageLabel">Age</label>
+                <input type="number" class="form-control" id="ageInput" rows="1" placeholder="42" />
+              </div>
+              <div class="form-group">
+                <label for="number" id="phoneLabel">Phone Number</label>
+                <input type="number" class="form-control" id="phoneInput" rows="1" placeholder="1235555555" />
+              </div>
+              <div class="form-group">
+                <label for="email" id="emailLabel">Email</label>
+                <input type="email" class="form-control" id="emailInput" rows="1" placeholder="example@gmail.com" />
+              </div>
+              <div class="form-group">
+                <label for="password" id="passwordLabel">Password</label>
+                <input type="password" class="form-control" id="passwordInput" rows="1" placeholder="Password" />
+              </div>
+              <div class="form-group">
+                <label for="photo" id="photoLabel">Photo</label>
+                <p id="photolabelP">Optional</p>
+                <button type="button" class="btn btn-secondary" id="uploadPhotoBtn">Upload</button>
+              </div>
+              <button type="submit" class="btn btn-primary btn-lg btn-block">Submit</button>
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 );
 
---
+++ client/src/components/UserProfile/UserProfile.js
@@ -0,0 +1,74 @@
+import React from "react";
+import "./UserProfile.css";
+
+<script>
+    firebase.auth().onAuthStateChanged(function (user) {
+        if (user) {
+        setTimeout(function () {
+            console.log("CURRENT USER FROM PROFILE PAGE", currentUser);
+            console.log("USER EMAIL", user.email);
+            $("#userName").text(currentUser.first_name + " " + currentUser.middle_name + " " + currentUser.last_name);
+            $("#userEmail").text(user.email);
+        }, 500);
+    }
+});
+</script>
+
+const UserProfile = props => (
+    <div className="container-fluid" id="userProfilePage">
+        <div className="row" id="userProfileRow1">
+            <div className="col-md-12">
+                <div className="pointsProgressBar"></div>
+            </div>
+        </div>
+        <hr />
+        <div className="row" id="userProfileRow2">
+            <div className="col-md-6 border-0 aos-item" id="userProfileColLeft" data-aos="flip-down">
+                <div className="card border-0" id="userPictureDiv">
+                    <div className="card-body border-0" id="userProfilePhotoCard">
+                        <img src="../img/users/gollum350x350.jpg" alt="Avatar" className="image img-fluid rounded-circle" id="userPicture" />
+                    </div>
+                    <div className="overlay border-0" id="userOverlay">
+                        <div className="userProfilePhotoBtn">
+                            <button type="button" className="btn btn-secondary" id="userProfilePhotoBtn">Update Image</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div className="col-md-6 media-body" id="userProfileColRight" data-aos="flip-up">
+                <ul id="userInfo" >
+                    <p>Name:
+                    <span id="userName"></span>
+                    </p>
+                    <p>Email:
+                    <span id="userEmail"></span>
+                    </p>
+                    <p>Points:
+                    <span id="userPoints"></span>
+                    </p>
+                    <a className="link" href="/challenges">View Challenges</a>
+                </ul>
+            </div>
+        </div>
+
+        <hr />
+
+        <div className="container-fluid" id="mapContainer">
+            <div className="row" id="mapBtnRow" data-aos="fade-right">
+                <div className="col-md-12" id="mapFloatingPanel">
+                    <button onclick="clearMarkers();" type="button btn-success" id="mapbtn">Hide Markers</button>
+                    <button onclick="showMarkers();" type="button btn-success" id="mapbtn">Show All Markers</button>
+                </div>
+            </div>
+            <div className="row" id="mapRow" data-aos="fade-left">
+                <div className="col-md-12" id="mapCol">
+                    <div id="map"></div>
+                </div>
+            </div>
+        </div>
+
+        <hr />
+    </div>
+);
+
+export default UserProfile;
