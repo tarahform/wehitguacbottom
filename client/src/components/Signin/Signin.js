2a754efe0eddc063b6a8bb61b7e0c3e725cd8ad0
Dalamo <dalamo90@gmail.com>
2018-06-27 11:44:48 -0500

    better sign up page

--- client/src/components/Signin/Signin.js
+++ client/src/components/Signin/Signin.js
@@ -2,22 +2,26 @@ import React from "react";
 import "./Signin.css";
 
 const Signin = () => (
-  <div className="col-md-12">
-    <form>
-      <div>
-        <label>User Name</label>
-        <input type="text" placeholder="Enter User Name" />
+  <div className="container-fluid" id="signinPage">
+    <div className="container-fluid" id="signinContainer">
+      <div className="row" id="signInRow">
+        <div className="col-md-12">
+          <form id="signInForm">
+            <div className="form-group">
+              <label htmlFor="exampleInputUserName" id="userNameLabel">User Name </label>
+              <input type="userName" className="form-control" id="loginUserNameInput" placeholder="Enter User Name" />
+            </div>
+            <div className="form-group">
+              <label htmlFor="exampleInputPassword" id="passwordLabel"> Password </label>
+              <input type="password" className="form-control" id="loginPasswordInput" placeholder="Enter Password" />
+            </div>
+            <div className="form-group">
+              <button type="signInSubmit" className="btn btn-primary"> Sign In </button>
+            </div>
+          </form>
+        </div>
       </div>
-      <div>
-        <label>Password</label>
-        <input type="password" placeholder="Enter Password" />
-      </div>
-      <div>
-        <button type="signInSubmit" className="btn btn-primary">
-        Sign In
-        </button>
-      </div>
-    </form>
+    </div>
   </div>
 );
 
