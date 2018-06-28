349e3b262b845dd395330a4a9a6297e6e992a453
SkyNet <nme312.noreply.github.com>
2018-06-28 15:56:34 -0500

    add components to survey

--- client/src/components/CheckBox/CheckBox.js
+++ client/src/components/CheckBox/CheckBox.js
@@ -6,7 +6,7 @@ const CheckBox = props => (
     <div className="form-check flavors">
         <input className="form-check-input" type="checkbox" value="" id={props.id} />
         <label className="form-check-label" htmlFor={props.id}>
-            {prop.label}
+            {props.label}
         </label>
     </div>
 )
--- client/src/views/SurveyForm/SurveyForm.js
+++ client/src/views/SurveyForm/SurveyForm.js
@@ -1,8 +1,9 @@
 import React, { Component } from "react";
 import CheckBox from "../../components/CheckBox";
-import RadioButton from "../../components/RadioButton";
-import friends from "../../flavors.json"
+// import RadioButton from "../../components/RadioButton";
+import flavors from "../../flavors.json"
 import "./SurveyForm.css";
+import RadioButton from "../../components/RadioButton/RadioButton";
 
 
 
@@ -16,9 +17,11 @@ import "./SurveyForm.css";
 
 class SurveyForm extends Component {
 
-    state = {
-      flavors
-    }
+  state = {
+    flavors,
+    subscriptions: ["Basic", "Moderate", "Premium"],
+    frequencies: ["Every Month", "Every 2 Months", "Every 3 Months"]
+  }
 
   render() {
     return (
@@ -26,9 +29,32 @@ class SurveyForm extends Component {
         <div className="row">
           <div className="col-md-12">
             <form>
-
-
-
+              <h2>What subscription level would you like?</h2>
+              {this.state.subscriptions.map((subscription, i) => (
+                <RadioButton
+                key={i}
+                id={"sub-"+i}
+                name={"sub"}
+                label={subscription}
+                />
+              ))}
+              <h2>How often would you like your delivery?</h2>
+              {this.state.frequencies.map((frequency, i) => (
+                <RadioButton
+                  key={i}
+                  id={"freq-"+i}
+                  name={"freq"}
+                  label={frequency}
+                  />
+              ))}
+              <h2>What flavors do you like?</h2>
+              {this.state.flavors.map((flavor, i) => (
+                <CheckBox
+                  key={i}
+                  id={"flav-"+i}
+                  label={flavor}
+                />
+              ))}
               <button className="btn btn-primary" type="submit">Submit</button>
             </form>
           </div>
