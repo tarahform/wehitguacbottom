91af8ac4b80feb9461915a71dbdf51580e096e23
Halle Conger <halle.conger@gmail.com>
2018-06-27 21:49:10 -0500

    flavors list functioning

--- client/src/components/AlcoholListItem/AlcoholListItem.js
+++ client/src/components/AlcoholListItem/AlcoholListItem.js
@@ -1,29 +1,36 @@
 import React from "react";
 import "./AlcoholListItem.css";
 
-const AlcoholListItem = props => (
-  <div className="card">
+const AlcoholListItem = props => {
+  // console.log(props.description);
+  // console.log(props.description.slice(2, -2));
+  const description = props.description.slice(2, -2).split("', '");
+  // console.log(description);
 
-    <div className="card-header" id="category">
-      {props.category}
-    </div>
+  return (
+    <div className="card">
 
-    <div className="card-body">
-      <h6 className="card-title" id="alcoholName">
-        <strong> {props.name} </strong>
-      </h6>
-      <div className="row">
-        <img className="col-md-6" id="alcoholImage" alt={props.name} src={`../../img/alcohol/${props.image}.png`} />
-        <ul className="col-md-6">
-          <li>
-            {props.description}
-          </li>
-        </ul>
+      <div className="card-header" id="category">
+        {props.category}
       </div>
-      <p className="card-text" id="price"> ${props.price} </p>
+
+      <div className="card-body">
+        <h6 className="card-title" id="alcoholName">
+          <strong> {props.name} </strong>
+        </h6>
+        <div className="row">
+          <img className="col-md-6" id="alcoholImage" alt={props.name} src={`../../img/alcohol/${props.image}.png`} />
+          <ul className="col-md-6">
+
+            {description.map((flavor, i) => <li key={i}>{flavor}</li>)}
+
+          </ul>
+        </div>
+        <p className="card-text" id="price"> ${props.price} </p>
+      </div>
+
     </div>
-    
-  </div>
-);
+  );
+}
 
 export default AlcoholListItem;
