b36114388dc3143ba0f21d62603eaeaefcb26652
SkyNet <nme312.noreply.github.com>
2018-06-27 22:47:44 -0500

    add user post route

--- routes/apiroutes.js
+++ routes/apiroutes.js
@@ -3,16 +3,14 @@ var db = require("../models");
 module.exports = app => {
     console.log("****************");
 
+    //get all alcohols in database
     app.get("/api/alcoholList", (req, res) => {
         db.Alcohol.findAll({}).then(data => {
             res.json(data);
         });
     });
 
-    // Project.findOne({ where: { title: 'aProject' } }).then(project => {
-    //     // project will be the first entry of the Projects table with the title 'aProject' || null
-    // })
-
+    //get all alcohols in a specific category
     app.get("/api/alcoholList/category/:category?", function (req, res) {
         db.Alcohol.findAll({
             where: {
@@ -25,6 +23,7 @@ module.exports = app => {
         });
     });
 
+    //get all alcohols in a specific subscription
     app.get("/api/alcoholList/subscription/:subscription?", function (req, res) {
         db.Alcohol.findAll({
             where: {
@@ -43,13 +42,33 @@ module.exports = app => {
         Map through each individual description array
         res.json(all description arrays that contain a given key word)
     */
+    //get all alcohols with a specific flavor description
     app.get("/api/alcoholList/description/:flavor", function (req, res) {
         db.Alcohol.findAll({
-            attributes: ["id", req.body.description]
+            attributes: ["id","alcohol_name","description"]
         }).then(function (data) {
             console.log("======================");
-            console.log(data);
+            data.map(data => {
+                console.log(data.description);
+
+            });
+            // console.log(data);
             // var newData = JSON.parse(data);
+            res.json(JSON.parse(data[0].description));
+        });
+    });
+
+    //add new users to the database
+    app.post("/api/users", function (req, res) {
+        db.User.create({
+            first_name: req.body.first_name,
+            middle_name: req.body.middle_name,
+            last_name: req.body.last_name,
+            photo_link: req.body.photo_link,
+            email: req.body.email,
+            phone_number: req.body.phone_number,
+            age: req.body.age,
+        }).then(function (data) {
             res.json(data);
         });
     });
