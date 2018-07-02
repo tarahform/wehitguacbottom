24c35a9730b6b0dde3028061d173cfb99c43c0c9
tarahform <ptarahform@gmail.com>
2018-07-02 14:54:20 -0500

    welcome page changed

--- client/src/views/About/About.js
+++ client/src/views/About/About.js
@@ -7,14 +7,14 @@ const About = () => (
     <div className="row text-center">
       <div className="col-md-6 text-center" id="mission">
         <h1 id="missionH1">Mission Statement</h1>
-        <p id="aboutText">Here at Create-OH, we focus on delivering <br />
+        <p id="aboutText1">Here at Create-OH, we focus on delivering <br />
         the best customer service and helping our fellow (of-age)<br />
          humans become more well rounded and connected in the world of drinking.
                 </p>
       </div>
       <div className="col-md-6 text-center" id="how">
         <h1 id="howItWorksH1">How It Works</h1>
-        <p id="aboutText">Create a profile.<br />
+        <p id="aboutText2">Create a profile.<br />
           Take a survey to help us better understand your tastes. <br />
           We send you 5 sample bottles of wine, beer and/or hard alcohol. <br />
           Recieve your box and enjoy. <br />
